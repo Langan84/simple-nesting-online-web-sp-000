682dcb492509336ee70d5779d9a28e6dd13af6da
langan.kg Langan <langan.kg@gmail.com>
2021-03-31 15:36:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
 
 		 programmer_hash
 		 {[:yukihiro_matsumoto][:known_for] = "Ruby",
-		  [:yukihiro_matsumoto][:languages] = "LISP", "C"
+		  [:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 		  }
 		 programmer_hash
 #ontacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
