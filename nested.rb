0b9e25ee219025e555c3d28a29acbe64998256fe
langan.kg Langan <langan.kg@gmail.com>
2021-03-31 16:04:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,7 +103,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-		  alans_new_info = "GUI"
+		alans_new_info = "GUI"
     programmer_hash[:alan_key][:known_for] = "GUI"
 		programmer_hash#change what Alan Kay is :known_for to the value of the alans_new_info variable.
 
